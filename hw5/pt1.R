6cbc34c2087f13eb01762675fc664bfb00c21d79
detree <ssong18@illinois.edu>
2016-03-16 19:08:30 -0500

    all done

--- hw5/pt1.R
+++ hw5/pt1.R
@@ -7,6 +7,7 @@ library(matrixStats)
 library(qlcMatrix)
 #================================para====================================
 data_file <- "docword.nips.txt"
+vocab_file <- "vocab.nips.txt"
 clustern=30
 smooth_const=1
 #================================para end================================
@@ -27,15 +28,6 @@ delta=matrix(data=0, nrow = docn, ncol=clustern)
 for( i in 1:docn)
   delta[i,delta_init[i]]=1
 #generate pjk, prob of word k in cluster j
-# eachword_cnt<-array(data=0, dim=wordn)
-# eachword_cnt<-colSums(xik)
-# eachword_inj_cnt<-matrix(data=0, nrow=wordn, ncol=clustern)
-# for( i in 1:docn ){
-#   tcluster<-which(delta[i,]==1)
-#   for( k in 1:wordn ){
-#     eachword_inj_cnt[k,tcluster] = eachword_inj_cnt[k,tcluster] + xik[i, k]
-#   }
-# }
 pjk<-matrix(data=0, nrow=clustern, ncol=wordn)
 for( j in 1:clustern ){
   for( k in 1:wordn ){
@@ -43,28 +35,45 @@ for( j in 1:clustern ){
     pjk[j,k] = sum(xik[delta_init==j,k])
   }
 }
-pjk = pjk/rowSums(pjk)
+pjk<- pjk/rowSums(pjk)
 #generate pi_j
-PIj<-array(data=0, dim=clustern)
-PIj<-colSums(delta)/docn
+PIj<- array(data=0, dim=clustern)
+PIj<- colSums(delta)/docn
 Qold=1
 Qnew=0
 while(abs(Qold-Qnew)>0.01){
-  logAj <- xik %*% t(log(pjk)) + t( matrix(nrow = clustern ,ncol = docn, data = log(PIj) ) )
-  logAmax<-rowMax(logAj)@x
-  right <- array(data = 0, dim = docn)
+  logAj<- xik %*% t(log(pjk)) + t( matrix(nrow = clustern ,ncol = docn, data = log(PIj) ) )
+  logAmax<- rowMax(logAj)@x
+  #init for the log(sig(e^(logAj-logAmax)))
+  logpart<- array(data = 0, dim = docn)
   for(i in 1:docn){
-    right[i]<-logSumExp(logAj[i,]-logAmax[i])
+    logpart[i]<-logSumExp(logAj[i,]-logAmax[i])
   }
-  logwij<-logAj - matrix(data=logAmax, nrow=docn, ncol=clustern) - matrix(data=right, nrow=docn, ncol=clustern)
-  wij<-exp(logwij)
-  pjk_new <- t(wij) %*% xik
-  pjk_new <- pjk_new/rowSums(pjk_new)
+  logwij<- logAj - matrix(data=logAmax, nrow=docn, ncol=clustern) - matrix(data=logpart, nrow=docn, ncol=clustern)
+  wij<- exp(logwij)
+  pjk_new<- t(wij) %*% xik
+  pjk_new<- pjk_new/rowSums(pjk_new)
   pjk_new[is.na(pjk_new)] <- 0
-  pjk<-pjk_new
+  pjk<- pjk_new
+  PIj_new<- colSums(wij)/docn
+  PIj<- PIj_new
   Qold=Qnew
   Qnew=(sum(xik %*% t(log(pjk)) + t(matrix(data=log(PIj), nrow=clustern, ncol=docn))))
-  print(c(Qold, Qnew))
+  cat( sprintf("old=%f, new=%f\n", Qold, Qnew) )
 }
 
-
+#the graph for each topic's probability
+plot(c(1:clustern), PIj, xlab="cluster#", ylab="probability", type="h")
+#the top10 word for each cluster
+vocab<-read.csv(vocab_file, header=FALSE)
+pjkcpy<-pjk
+words<-matrix(data="", nrow=clustern, ncol=10)
+for(j in 1:clustern){
+  cat( sprintf("cluster%d: ", j) )
+  for(cnt in 1:10){
+    index = which.max(pjkcpy[j,])
+    cat( sprintf("%s, ", vocab[index,1]) )
+    pjkcpy[j,index]=-9999
+  }
+  cat( sprintf("\n") )
+}
