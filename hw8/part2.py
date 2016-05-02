c7e9da60866e6713396ad242037db787c3601e2c
Shuchen Song <ssong18@illinois.edu>
2016-05-02 00:28:24 -0500

    few change

--- hw8/part2.py
+++ hw8/part2.py
@@ -29,20 +29,20 @@ x_image = tf.reshape(x, [-1,28,28,1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 #h_pool1 = max_pool_2x2(h_conv1)
 #layer-2======================================
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5, 5, 32, 32])
+b_conv2 = bias_variable([32])
 h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 #layer-3======================================
-W_conv3 = weight_variable([5, 5, 64, 128])
-b_conv3 = bias_variable([128])
+W_conv3 = weight_variable([5, 5, 32, 64])
+b_conv3 = bias_variable([64])
 h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
 h_pool3 = max_pool_2x2(h_conv3)
 #densely connected layer======================
-W_fc1 = weight_variable([7 * 7 * 128, 1024])
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool3_flat = tf.reshape(h_pool3, [-1, 7*7*128])
+h_pool3_flat = tf.reshape(h_pool3, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
@@ -61,7 +61,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.initialize_all_variables())
 for i in range(2000):
   batch = mnist.train.next_batch(50)
-  if i%100 == 0:
+  if i%10 == 0:
     train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
