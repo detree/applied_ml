c8d855d5d6c043261a5f548b9f1f876bc05ae95a
lunanli3 <lunanli3@illinois.edu>
2016-05-04 19:57:05 -0500

    Add readme, group.txt and cifar10 2 layer code

--- hw8/tutorial/visual.py
+++ hw8/tutorial/visual.py
@@ -32,7 +32,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data '
                      'for unit testing.')
-flags.DEFINE_integer('max_steps',2000, 'Number of steps to run trainer.')
+flags.DEFINE_integer('max_steps',3000, 'Number of steps to run trainer.')
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_float('dropout', 0.9, 'Keep probability for training dropout.')
 flags.DEFINE_string('data_dir', '/tmp/data', 'Directory for storing data')
